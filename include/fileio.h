236688a53f447db7a3607267fbdc98ac2cafdc42
jimmy-print <jimmy.wang123@outlook.sg>
2020-12-18 18:38:20 +0800

    rm unused func and includes

--- include/fileio.h
+++ include/fileio.h
@@ -2,7 +2,6 @@
 #ifndef FILEIO_H
 #define FILEIO_H
 
-int get_file_length(const char *file);
 std::string get_file_str(const char *file);
 
 #endif
--- src/fileio.cpp
+++ src/fileio.cpp
@@ -1,26 +1,7 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
 #include <fstream>
 #include <string>
 #include <iostream>
 
-
-int get_file_length(const char *file)
-{
-	FILE *f = fopen(file, "r");
-	if (f == NULL) {
-		printf("File opening error\n");
-		abort();
-	}
-	fseek(f, 0, SEEK_END);
-	int size = ftell(f);
-	fseek(f, 0, SEEK_SET);
-	fclose(f);
-	
-	return size;
-}
 
 std::string get_file_str(const char *file)
 {
