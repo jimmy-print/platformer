15ccdfe6157f2c51f75dc91f6a57031b5b610cb9
jimmy-print <jimmy.wang123@outlook.sg>
2020-12-29 09:35:56 +0800

    simplify shader function

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,13 +1,5 @@
 #include <GL/glew.h>
 
-GLuint compileshader(GLuint type, const char* source_string)
-{
-	unsigned int id = glCreateShader(type);
-	glShaderSource(id, 1, &source_string, NULL);
-	glCompileShader(id);
-	return id;
-}
-
 
 GLuint loadshader(const char *vs_cstr, const char *fs_cstr)
 {
@@ -15,8 +7,13 @@ GLuint loadshader(const char *vs_cstr, const char *fs_cstr)
 
 	program = glCreateProgram();
 
-	GLuint vs = compileshader(GL_VERTEX_SHADER, vs_cstr);
-	GLuint fs = compileshader(GL_FRAGMENT_SHADER, fs_cstr);
+	GLuint vs, fs;
+	vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vs_cstr, NULL);
+	glCompileShader(vs);
+	fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fs_cstr, NULL);
+	glCompileShader(fs);
 
 	glAttachShader(program, vs);
 	glAttachShader(program, fs);
